adea1d94ce3247665f730549327ca0a324b47f9a
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-27 14:20:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const  num1 = 62
+const  num2 = 1
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 10 + 2)
+const mod = 16%6
+const max = Math.max(1, 20)
