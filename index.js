608137ad0d875c9b3c7596c34983b6f6b2c36c70
github username <github email address>
2020-01-27 14:53:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 24 % 5;
+
+
+let max = Math.max(1, 3, 5, 20, 8);
