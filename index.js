fc31b87df9e5a59eb50db49b0828a1d7028dfd03
John Louis Rumingan <jmrumingan@gmail.com>
2020-01-27 16:56:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const numArray = [100, 30, 24, 50];//
+
+let multiply = num1 * num2;
+
+let random =  Math.floor(Math.random() *10) + 1;
+
+let mod = 34 % 10 ;
+
+let max = Math.max(5, 20, 13, 15);
